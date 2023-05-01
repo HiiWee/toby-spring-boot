838b7260107933513d85eb3fb1e710c05b4dc5c1
HiiWee <wpdlzhf159@gmail.com>
2023-05-02 03:11:14 +0900

    feat: Hello 리포지토리

---
+++ hoseok/helloboot/src/main/java/tobyspring/helloboot/HelloRepository.java
@@ -0,0 +1,17 @@
+package tobyspring.helloboot;
+
+public interface HelloRepository {
+
+    Hello findHello(String name);
+
+    void increaseCount(String name);
+
+    default int countOf(String name) {
+        Hello hello = findHello(name);
+        if (hello == null) {
+            return 0;
+        }
+        return hello.getCount();
+    }
+
+}
